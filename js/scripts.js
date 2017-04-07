a5734ea97fb8e112eafa00496d30167021734535
Grady L Shelton <gradyish@gmail.com>
2017-04-07 10:01:16 -0700

    test size function and prototype method to adjust pizza price based on size

--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,35 @@
 // Business Logic
-function User(name) {
-  this.name = name;
-}
 
-function Pizza(size){
+function Pizza(name, size){
+  this.name = name;
   this.size = size;
   this.frills = [];
 }
 
-Pizza.prototype.sizeCost = function(size) {
+// test works!
+var sizeCost = function(size) {
   var sizePrice = 0;
-  if (this.size === "small") {
+  if (size === "small") {
     sizePrice = 0;
-  } else if (this.size === "medium") {
+  } else if (size === "medium") {
     sizePrice = 2;
-  } else {
+  } else if (size === "large"){
     sizePrice = 4;
   }
   return sizePrice;
 }
 
-Pizza.prototype.frillsCost = function(frills) {
+var frillsCost = function(frills) {
   let frillsPrice = 0;
-  for(i=0; i<this.frills.length; i++) {
+  for(i=0; i<=this.frills.length; i++) {
     frillsPrice += this.frills[i];
   }
   return frillsPrice;
 }
 
-//   var frillsPrices = [];
-//   var frillsPrice = 0;
-//   var totalFrillsPrice = 0;
-//   for(i=0; i<this.frills.length; i++){
-//     if (this.frills[i] === "meat") {
-//       frillsPrice = 1.5;
-//       frillsPrices.push(thisfrills[i])
-//     } else {
-//       frillsPrice = 0.75;
-//     }
-//     return frillsPrice;
-//   }
-//   for(i=0; i<frillsPrices.length; i++){
-//     totalFrillsPrice += frillsPrices[i];
-//   }
-// }
-
-Pizza.prototype.pizzaCost = function(sizePrice, totalFrillsPrice){
-  var price = 9 + sizeCost(size) + frillsCost(frills);
+Pizza.prototype.pizzaCost = function(size){
+  var price = 9 + sizeCost(size);
+  //  + frillsCost(frills);
   return "$" + price + ".00";
 }
 
@@ -55,20 +38,17 @@ Pizza.prototype.pizzaCost = function(sizePrice, totalFrillsPrice){
 $(function(){
   $("#mainForm").submit(function(e){
     e.preventDefault();
-    let name = $("#name").val();
-    let size = $("#sizes").val();
-    let pizza = new Pizza(size);
+    var name = $("#name").val();
+    var size = $("#sizes").val();
+    var pizza = new Pizza(name, size);
     $("input:checkbox[name=frills]:checked").each(function(){
     let inputFrills = parseFloat($(this).val());
     pizza.frills.push(inputFrills);
     console.log(pizza.frills);
     });
+
+    let totalCost = pizza.pizzaCost(pizza.size);
+    $(".result").show();
+    $("#price-result").text(totalCost);
   });
 });
-
-
-// checkbox data gather example
-// $("input:checkbox[name=work-transportation]:checked").each(function(){
-//      var workTransportationMode = $(this).val();
-//      $('#work-responses').append(workTransportationMode + "<br>");
-//    });
